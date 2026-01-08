3a88aaa8403e3eb89b5b0138155ad414159aa0bc
Lucas-Ribeiro-Lima <lucasribeirolima974@gmail.com>
2026-01-08 20:47:23 -0300

    refactor: Scene update after instances remove.

--- include/Scene.h
+++ include/Scene.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #include <Camera.h>
-#include <Instances.h>
+#include <Entity.h>
 
 namespace oriongl::core {
 
 struct Scene {
-    std::vector<Instances> data;
+    std::vector<Entity> data;
     std::vector<graphics::Light> lights;
     Camera camera;
 };
